fec93bca7587a8197a847d5f55f521233e06f453
Ahmed Tarek <ahmedtarekmatouk@gmail.com>
2026-07-23 12:18:31 +0300

    domain: add BaseEntity, Product and Category entities

---
+++ src/InventoryManagement.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using InventoryManagement.Domain.Common;
+
+namespace InventoryManagement.Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ src/InventoryManagement.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InventoryManagement.Domain.Common;
+
+namespace InventoryManagement.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public bool IsDeleted { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
